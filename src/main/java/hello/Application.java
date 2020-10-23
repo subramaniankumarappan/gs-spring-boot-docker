ee6773768b205ad80d0ee2717e3342b6b6a400c2
subramanian.kumarappan <subramanian.kumarappan@cognizant.com>
2020-10-23 09:45:32 +0530

    test build

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-	@RequestMapping("/")
+	@RequestMapping("/")   
 	public String home() {
 		return "Hello Docker World";
 	}
